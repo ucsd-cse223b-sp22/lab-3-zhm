3e9155e47e6e6a96e604fcae548b7f27c0ca4a43
HeidiCheng <heidi8844@gmail.com>
2022-05-06 08:51:29 -0700

    Update migration draft

--- lab/src/lab3/migration.rs
+++ lab/src/lab3/migration.rs
@@ -12,7 +12,6 @@ use tribbler::{
     storage::{KeyList, KeyString, KeyValue, List, Pattern, Storage},
 };
 
-// const USER_EXISTS_KEY: &str = "_existing_user_#";
 const PREFIX: &str = "PREFIX_";
 const SUFFIX: &str = "SUFFIX_";
 
@@ -37,7 +36,7 @@ pub struct BackendEvent {
 
 async fn fetch_all_bins(storage: Arc<StorageClient>) -> TribResult<Vec<String>> {
     // fetch both keys and list_keys => do it seperately
-    // fatch all keys whose format would be "bin_name::", and get the bin_name
+    // fatch all keys whose format would be "bin_name::key", and get the bin_name
     let all_keys = match storage
         .keys(&Pattern {
             prefix: "".to_string(),
@@ -114,7 +113,7 @@ async fn migration_join(
     new: usize,
     live_https: Vec<usize>,
     storage_clients: Vec<Arc<StorageClient>>,
-    // <bin, vector of migrated keys>
+    // <bin, vector of migrated keys> -> prevent redundant operations
     migration_log: HashMap<String, Vec<String>>,
 ) -> TribResult<bool> {
     let live_https_len = live_https.len();
@@ -197,6 +196,7 @@ async fn migration_crash(
     crashed: usize,
     live_https: Vec<usize>,
     storage_clients: Vec<Arc<StorageClient>>,
+    // <bin, vector of migrated keys> -> prevent redundant operations
     migration_log: HashMap<String, Vec<String>>,
 ) -> TribResult<bool> {
     let live_https_len = live_https.len();
@@ -221,7 +221,7 @@ async fn migration_crash(
         Err(err) => return Err(err),
     };
 
-    // Move all the user data who hashed into the range (id of node before predecessor, id of predecessor] to crashed node's successor
+    // Move all the data in bin which is hashed into the range (id of node before predecessor, id of predecessor] to crashed node's successor
     for bin in pred_bins.iter() {
         let idx = hash_bin_name_to_backend_idx(bin, &live_https, storage_clients.len()).await?;
         if check_in_bound_wrap_around(prev_pred, pred, idx) {
@@ -239,7 +239,7 @@ async fn migration_crash(
         }
     }
 
-    // Move all the user data who hashed into the range (id of predecessor, crashed_node] to the node succeed crashed node's successor
+    // Move all the data in bin which is hashed into the range (id of predecessor, crashed_node] to the node succeed crashed node's successor
     for bin in succ_bins.iter() {
         let idx = hash_bin_name_to_backend_idx(bin, &live_https, storage_clients.len()).await?;
         if check_in_bound_wrap_around(pred, crashed, idx) {
@@ -264,9 +264,12 @@ async fn bin_migration(
     bin_name: &str,
     from: Arc<StorageClient>,
     to: Arc<StorageClient>,
+    // <vector of migrated keys> -> prevent redundant operations
     migration_log: Vec<String>,
 ) -> TribResult<bool> {
+
     // Copying KeyValue pair
+
     // How keeper record this part? It seems okay to redo all set operation
     // However, it seems not that efficient
     let keys = from
@@ -278,7 +281,6 @@ async fn bin_migration(
         .0;
 
     // Get value from storage and append it to backend
-    //tokio::spawn(async move {
     for key in keys.iter() {
         let value = match from.get(key).await {
             Ok(Some(val)) => val,
@@ -296,11 +298,17 @@ async fn bin_migration(
             Err(_) => return Err("Backend crashed when doing migration".into()),
         }
     }
-    //});
 
     // Copying list
-    // Should be prefix_username::trib, prefix_username::follow_log
+    
     // Keeper sends acknowledgement when receiving both prefix and suffix log
+    // Record bin_name::key part to group the operations for the same keys together
+    let mut list_keys_hs = HashSet<String>::new(); 
+
+    // For stripping PREFIX_ and SUFFIX_  
+    let prefix_len = PREFIX.len(); 
+
+    // Should be PREFIX_prefix_username::trib, PREFIX_prefix_username::follow_log
     let prefix_list_keys = from
         .list_keys(&Pattern {
             prefix: format!("{}{}::", PREFIX, &bin_name.clone().to_string()),
@@ -308,50 +316,46 @@ async fn bin_migration(
         })
         .await?
         .0;
-
-    for key in prefix_list_keys.iter() {
-        //tokio::spawn(async move {
-        let list = match from.list_get(key).await {
-            Ok(List(vec)) => vec,
-            Err(_) => return Err("Backend crashed when doing migration".into()),
-        };
-        for item in list.iter() {
-            match to
-                .list_append(&KeyValue {
-                    key: key.clone(),
-                    value: item.clone(),
-                })
-                .await
-            {
-                Ok(_) => (),
-                Err(_) => return Err("Backend crashed when doing migration".into()),
-            }
-        }
-
-        // Keeper sends keys to other keeper to acknowledge they finished copying this list
-        // send_key(key);
-        //})
-    }
-
     // Should be suffix_username::trib, suffix_username::follow_log
     let suffix_list_keys = from
         .list_keys(&Pattern {
-            prefix: format!("{}{}::", SUFFIX, &bin_name.clone().to_string()),
-            suffix: "".to_string(),
+        prefix: format!("{}{}::", SUFFIX, &bin_name.clone().to_string()),
+        suffix: "".to_string(),
         })
         .await?
         .0;
 
+    for key in prefix_list_keys.iter() {
+        let untyped_list_key = &key[prefix_len..];
+        list_keys_hs.insert(untyped_list_key);  
+    }
+
     for key in suffix_list_keys.iter() {
-        //tokio::spawn(async move {
-        let list = match from.list_get(key).await {
+        let untyped_list_key = &key[prefix_len..];
+        list_keys_hs.insert(untyped_list_key);  
+    }
+
+    let list_keys: Vec<String> = list_keys_hs.into_iter().collect();
+
+    let mut migration_log_into_iter = migration_log.into_iter(); 
+    
+    for key in list_keys.iter() {
+        
+        // Skip the key that has been migrated before
+        if migration_log_into_iter.find(|&x| x == key) {
+            continue; 
+        } 
+
+        let prefix_key = format!("{}{}", PREFIX, key); 
+
+        let prefix_key_list = match from.list_get(&prefix_key).await {
             Ok(List(vec)) => vec,
             Err(_) => return Err("Backend crashed when doing migration".into()),
         };
-        for item in list.iter() {
+        for item in prefix_key_list.iter() {
             match to
                 .list_append(&KeyValue {
-                    key: key.clone(),
+                    key: prefix_key.clone(),
                     value: item.clone(),
                 })
                 .await
@@ -361,9 +365,25 @@ async fn bin_migration(
             }
         }
 
+        let suffix_key = format!("{}{}", SUFFIX, key); 
+        let suffix_key_list = match from.list_get(&suffix_key).await {
+            Ok(List(vec)) => vec,
+            Err(_) => return Err("Backend crashed when doing migration".into()),
+        };
+        for item in suffix_key_list.iter() {
+            match to
+                .list_append(&KeyValue {
+                    key: suffix_key.clone(),
+                    value: item.clone(),
+                })
+                .await
+            {
+                Ok(_) => (),
+                Err(_) => return Err("Backend crashed when doing migration".into()),
+            }
+        }
         // Keeper sends keys to other keeper to acknowledge they finished copying this list
         // send_key(key);
-        //})
     }
 
     Ok(true)
